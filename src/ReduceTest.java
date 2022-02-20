6f0a641d6fc83cdb8722cb649c371065760a12be
younggyo <lee4519698@hanmail.net>
2022-02-21 00:10:44 +0900

    Initial commit

---
+++ src/ReduceTest.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+
+class Comparing implements BinaryOperator<String>{
+
+    @Override
+    public String apply(String s1, String s2) {
+        if (s1.getBytes().length >= s2.getBytes().length) return s1;
+        else return s2;
+    }
+}
+
+public class ReduceTest {
+    public static void main(String[] args) {
+        String[] greetings = {"한글자", "한두글자", "한두세글자"};
+        System.out.println(Arrays.stream(greetings).reduce("", (s1, s2)-> {
+            if (s1.getBytes().length >= s2.getBytes().length) return s1;
+            else return s2;
+        }));
+
+        String str = Arrays.stream(greetings).reduce(new Comparing()).get();
+        System.out.println(str);
+    }
+}
